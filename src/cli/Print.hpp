535296f359ad797f7f30f9d4819472168576a0b0
Juraj Bencurik <bencurikjuraj@gmail.com>
2025-11-06 10:02:46 +0100

    feat: add cli print function

---
+++ src/cli/Print.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "data/models/Spell.hpp"
+#include "data/models/SpellSchool.hpp"
+
+inline std::string truncate(const std::string& str, std::size_t maxLen) {
+    if (str.size() <= maxLen) return str;
+    return str.substr(0, maxLen - 3) + "...";
+}
+
+inline void printSpellsTable(const std::vector<Spell>& spells) {
+    std::cout << "LVL  | SCHOOL         | NAME                 | CAST TIME  | "
+                 "RANGE    | COMPONENTS\n";
+    std::cout << "---- | -------------- | -------------------- | ---------- | "
+                 "-------- | ----------\n";
+    for (const auto& s : spells) {
+        std::cout.width(2);
+        std::cout << s.level << "   | ";
+        std::cout.width(14);
+        std::cout << std::left << to_string(s.school) << " | ";
+        std::cout.width(20);
+        std::cout << truncate(s.name, 20) << " | ";
+        std::cout.width(10);
+        std::cout << truncate(s.cast_time, 10) << " | ";
+        std::cout.width(8);
+        std::cout << s.range << " | ";
+        std::cout.width(10);
+        std::cout << s.components << "\n";
+    }
+}
